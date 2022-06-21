c8dcc6685d284633d39d45038d63011b4f1b9faf
zenghui <410486047@qq.com>
2022-06-21 09:23:48 +0800

    init project

---
+++ js/jsbestpractise7.js
@@ -0,0 +1,21 @@
+window.onload = prepareLinks;
+function prepareLinks() {
+    if (!document.getElementsByTagName) {
+        return false;
+    }
+    var links = document.getElementsByTagName("a");
+    if (links.length > 0) {
+        for (var i = 0; i < links.length; i++) {
+            if (links[i].getAttribute("class") == "popUp") {
+                links[i].onclick = function () {
+                    popUp(this.getAttribute("href"));
+                    return false;
+                }
+            }
+        }
+    }
+}
+
+function popUp(winURL) {
+    window.open(winURL, "popup", "width=320,height=480");
+}
